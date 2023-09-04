93d6e8138fc4764cd49eb1e64e42eda255320d71
Phumlani Dlamini <phumlanibrightdlamini@gmail.com>
2023-09-04 16:10:39 +0200

    linked firebase

--- app.js
+++ app.js
@@ -1,43 +1,4 @@
 
-// // Import the functions you need from the SDKs you need
-// import { initializeApp } from "https://www.gstatic.com/firebasejs/10.3.0/firebase-app.js";
-// import { getAnalytics } from "https://www.gstatic.com/firebasejs/10.3.0/firebase-analytics.js";
-// // TODO: Add SDKs for Firebase products that you want to use
-// // https://firebase.google.com/docs/web/setup#available-libraries
-
-// // Your web app's Firebase configuration
-// // For Firebase JS SDK v7.20.0 and later, measurementId is optional
-// const firebaseConfig = {
-//     apiKey: "AIzaSyCPLEa1Od7Z3b4gVcA5hoUKNImd8DKw0kk",
-//     authDomain: "awesomeportfolio-6388a.firebaseapp.com",
-//     projectId: "awesomeportfolio-6388a",
-//     storageBucket: "awesomeportfolio-6388a.appspot.com",
-//     messagingSenderId: "79935082843",
-//     appId: "1:79935082843:web:ce82d45eb23ddc72390a7e",
-//     measurementId: "G-K9FZCK5DCN"
-// };
-
-// // Initialize Firebase
-// firebase.initializeApp(firebaseConfig);
-// const app = initializeApp(firebaseConfig);
-// const analytics = getAnalytics(app);
-
-// document.querySelector('.submit').addEventListener('click', function () {
-//     // Get the form element
-//     const form = document.getElementById("contactForm");
-
-//     // Get the form data
-//     const name = document.getElementById("name").value;
-//     const email = document.getElementById("email").value;
-//     const message = document.getElementById("message").value;
-
-//     // Add the form data to the Firebase database
-//     firebase.database().ref("contact").push({
-//         name: name,
-//         email: email,
-//         message: message
-//     });
-// });
 
 
 
